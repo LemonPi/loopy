0cfdb72899e187e673544878b9ef9f5f2db42951
Johnson Zhong <johnsonzhong@hotmail.ca>
2018-02-17 12:41:04 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const dp = drawpoint;
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+const W = screen.width;
+const H = screen.height;
+
+canvas.id = "display";
+canvas.width = W;
+canvas.height = H;
+document.body.appendChild(canvas);
+
+
+const strings = {
+    duration  : 10,
+    numPts    : 10,
+    numStrings: 1,
+    size      : 1,
+    order     : 1,
+    smoothness: 0,
+
+    pts: [],
+    initPts() {
+        ctx.clearRect(0, 0, W, H);
+        this.pts = [];
+        for (let i = 0; i < this.numPts; ++i) {
+            this.pts.push(dp.point(Math.random() * W, Math.random() * H));
+            // TODO limit size of randomness by size
+        }
+        // reduce to a nicer initial shape by being convex and non-intersecting
+        this.pts = convexHull(this.pts);
+        // TODO add control points for each point corresponding to order
+
+        ctx.beginPath();
+        dp.drawPoints(ctx, ...this.pts, this.pts[0]);
+        ctx.stroke();
+
+        // TODO calculate positions of points over time with constraint of being back after duration
+    },
+
+    // TODO animate
+
+    reroll() {
+        console.log('reroll');
+        this.initPts();
+    },
+    "save to GIF"() {
+        console.log("save to GIF");
+        // TODO
+    }
+};
+
+strings.initPts();
+
+const gui = new dat.GUI();
+gui.add(strings, 'duration', 0, 20);
+gui.add(strings, 'numPts', 0, 20);
+gui.add(strings, 'numStrings', 1, 3);
+gui.add(strings, 'size', 0, 1);
+gui.add(strings, 'order', 1, 3);
+gui.add(strings, 'smoothness', 0, 5);
+gui.add(strings, 'reroll');
+gui.add(strings, 'save to GIF');
+
+
